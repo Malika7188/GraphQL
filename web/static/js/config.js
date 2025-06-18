9dd9a23c97d868ef8a212dde9d24cda3f15eff77
masman <asmanodeny@gmail.com>
2025-06-18 17:06:18 +0300

    updated config files

--- web/static/js/config.js
+++ web/static/js/config.js
@@ -1,12 +1,12 @@
 // Configuration settings
-const CONFIG = {
+export const CONFIG = {
     GRAPHQL_ENDPOINT: 'https://learn.zone01kisumu.ke/api/graphql-engine/v1/graphql',
     SIGNIN_ENDPOINT: 'https://learn.zone01kisumu.ke/api/auth/signin',
     XP_PER_LEVEL: 1000
 };
 
 // Application state
-let appState = {
+export let appState = {
     token: null,
     userData: null
 };
